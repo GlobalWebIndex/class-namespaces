6ffa8453ac8eddc6b98722bfe23211f223c0afdf
Marek Fajkus <marek.faj@gmail.com>
2017-05-14 19:48:28 +0200

    initial implementation (#1)

---
+++ src/NamespaceClass.elm
@@ -0,0 +1,156 @@
+module NamespaceClass
+    exposing
+        ( ClassName
+        , namespace
+        , element
+        , toClass
+        , nested
+        , withStates
+        )
+
+{-| Abstraction for generating convetional class names
+
+# Type and Constructor
+
+@docs ClassName, namespace
+
+# Adding Elements
+
+@docs element
+
+# Convert to Attribute
+
+@docs toClass, element, nested, withStates
+
+-}
+
+import Html exposing (Attribute)
+import Html.Attributes exposing (class)
+
+
+-- Type
+
+
+{-| ClassName type describes selector used to style element.
+-}
+type ClassName
+    = ClassName String (List String)
+
+
+{-| Construct [`ClassName`](#ClassName) with given namespace
+
+    >>> import Html.Attributes exposing (class)
+
+    >>> namespace "menu" |> toClass
+    class "menu"
+-}
+namespace : String -> ClassName
+namespace name =
+    ClassName name []
+
+
+
+-- Add nested element
+
+
+{-| Add element
+
+    >>> import Html.Attributes exposing (class)
+
+    >>> namespace "menu"
+    ... |> element "list"
+    ... |> toClass
+    class "menu__list"
+-}
+element : String -> ClassName -> ClassName
+element name (ClassName namespace list) =
+    ClassName namespace <| name :: list
+
+
+
+-- Convert to Html.Attribute
+
+
+{-| Convert [`ClassName`](#ClassName) to `Html.Attribute msg`
+
+    >>> import Html.Attributes exposing (class)
+
+    >>> namespace "menu"
+    ... |> toClass
+    class "menu"
+
+    >>> namespace "menu"
+    ... |> element "list"
+    ... |> toClass
+    class "menu__list"
+-}
+toClass : ClassName -> Attribute msg
+toClass =
+    class << toString
+
+
+{-| Add new element and turn to `Html.Attribute msg`
+
+    >>> import Html.Attributes exposing (class)
+
+    >>> namespace "menu"
+    ... |> nested "item"
+    class "menu__item"
+
+    >>> namespace "menu"
+    ... |> element "item"
+    ... |> nested "link"
+    class "menu__item--link"
+-}
+nested : String -> ClassName -> Attribute msg
+nested name =
+    toClass << element name
+
+
+{-| Add state to [`ClassName`](#ClassName) and turn to `Html.Attrinute msg`
+
+    >>> import Html.Attributes exposing (class)
+
+    >>> namespace "menu"
+    ... |> element "item"
+    ... |> withStates ["active", "highlighted"]
+    class "menu__item active highlighted"
+
+    >>> namespace "menu"
+    ... |> withStates []
+    class "menu"
+-}
+withStates : List String -> ClassName -> Attribute msg
+withStates state =
+    class << (toStringWithStates state)
+
+
+
+-- Private
+
+
+toString : ClassName -> String
+toString (ClassName namespace list) =
+    let
+        spacer acc =
+            if String.isEmpty acc then
+                ""
+            else
+                "--"
+
+        addElement name acc =
+            acc ++ (spacer acc) ++ name
+    in
+        case list of
+            [] ->
+                namespace
+
+            head :: tail ->
+                namespace
+                    ++ "__"
+                    ++ List.foldr addElement "" list
+
+
+toStringWithStates : List String -> ClassName -> String
+toStringWithStates states className =
+    toString className ++ List.foldl (\s acc -> acc ++ " " ++ s) "" states
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,34 +4,53 @@ import Test exposing (..)
 import Expect
 import Fuzz exposing (list, int, tuple, string)
 import String
+import Html.Attributes exposing (class)
+
+
+-- lib
+
+import NamespaceClass exposing (..)
+
+
+namespaced =
+    namespace "menu"
+
+
+inElement =
+    element "list" namespaced
 
 
 all : Test
 all =
-    describe "Sample Test Suite"
-        [ describe "Unit test examples"
-            [ test "Addition" <|
-                \() ->
-                    Expect.equal (3 + 7) 10
-            , test "String.left" <|
-                \() ->
-                    Expect.equal "a" (String.left 1 "abcdefg")
-            , test "This test should fail - you should remove it" <|
-                \() ->
-                    Expect.fail "Failed as expected!"
-            ]
-        , describe "Fuzz test examples, using randomly generated input"
-            [ fuzz (list int) "Lists always have positive length" <|
-                \aList ->
-                    List.length aList |> Expect.atLeast 0
-            , fuzz (list int) "Sorting a list does not change its length" <|
-                \aList ->
-                    List.sort aList |> List.length |> Expect.equal (List.length aList)
-            , fuzzWith { runs = 1000 } int "List.member will find an integer in a list containing it" <|
-                \i ->
-                    List.member i [ i ] |> Expect.true "If you see this, List.member returned False!"
-            , fuzz2 string string "The length of a string equals the sum of its substrings' lengths" <|
-                \s1 s2 ->
-                    s1 ++ s2 |> String.length |> Expect.equal (String.length s1 + String.length s2)
-            ]
+    describe "Namespace Class"
+        [ test "block" <|
+            \() ->
+                toClass namespaced
+                    |> Expect.equal (class "menu")
+        , test "element" <|
+            \() ->
+                toClass inElement
+                    |> Expect.equal (class "menu__list")
+        , test "wclass" <|
+            \() ->
+                nested "item" inElement
+                    |> Expect.equal (class "menu__list--item")
+        , test "more elements" <|
+            \() ->
+                inElement
+                    |> element "item"
+                    |> nested "link"
+                    |> Expect.equal (class "menu__list--item--link")
+        , test "with state" <|
+            \() ->
+                inElement
+                    |> element "item"
+                    |> withStates ["active", "edited"]
+                    |> Expect.equal (class "menu__list--item active edited")
+        , test "with empty state" <|
+            \() ->
+                inElement
+                    |> element "item"
+                    |> withStates []
+                    |> Expect.equal (class "menu__list--item")
         ]
